6870cc4b4075f5f3fd20e302b35052082bd2953b
Mustafa <m.mufeed1996@gmail.com>
2019-06-28 20:22:01 +0200

    fixed auth SP

--- LoginProcedure.sql
+++ LoginProcedure.sql
@@ -1,5 +1,5 @@
 GO
-CREATE PROC usp_Employee_Login 
+CREATE OR ALTER PROC usp_Employee_Login 
 	@EmailOrPAN NVARCHAR(128),
 	@HashPassword NVARCHAR(128),
 	@return_Hex_value NVARCHAR(2)='FF' OUTPUT,
@@ -18,56 +18,63 @@ BEGIN
 				IF EXISTS (SELECT TOP 1 EID FROM Employee WHERE Email=@EmailOrPAN OR PAN = @EmailOrPAN OR NationalID=@EmailOrPAN)
 				-- Found the user using email or PAN or National ID
 					BEGIN
-						SET @userID = (SELECT EID FROM Employee WHERE (Email=@EmailOrPAN OR PAN = @EmailOrPAN OR NationalID=@EmailOrPAN) AND HashPassword=@HashPassword)
+						SET @userID = (SELECT EID FROM Employee WHERE (Email=@EmailOrPAN OR PAN = @EmailOrPAN OR NationalID=@EmailOrPAN) AND (HashPassword=@HashPassword))
 						IF(@userID IS NULL)
-						-- Worng Password
+						-- Wrong Password
 							BEGIN
 								SET @responseMessage='Incorrect password'
 								SELECT @return_Hex_value = '02'
-								return 2
 							END
 						ELSE
+						BEGIN
 						-- Correct password, so check if he's already logged in
-							SET @status=(SELECT EmpolyeeStatus from Employee WHERE EID=@userID)
+							SET @status=(SELECT EmployeeStatus from Employee WHERE EID=@userID)
 							IF(@status = 0)
-							-- Not logged in, so login successful, send his type to backend
+							-- Not logged in, so login successful, send his type to backend and jobID
 							-- And set status to 1
 								BEGIN
-									SET @responseMessage='User successfully logged in'
+									SET @responseMessage='User logged in successfully'
 									SELECT @return_Hex_value = '00'
 									SET @JobID = (SELECT JobID from Employee where EID = @userID)
 									SET @employeeID = @userID
-									UPDATE Employee SET EmpolyeeStatus = 1 WHERE EID = @userID
+									UPDATE Employee SET EmployeeStatus = 1 WHERE EID = @userID
 									UPDATE Employee SET LogInTStamp = GETDATE() WHERE EID = @userID
-									return 0
 								END
 							ELSE IF(@status = 1)
 							-- Already logged in, so can't continue
 								BEGIN
 									SET @responseMessage='User is logged in somewhere'
 									SELECT @return_Hex_value = '03'
-									return 3
 								END
-						ELSE
-						BEGIN
-						   SET @responseMessage='Something went wrong'
-						   SELECT @return_Hex_value = 'ff'
-						END		
+							ELSE IF(@status = 2)
+							-- Not verrified
+								BEGIN
+									SET @responseMessage='This user is not verified'
+									SELECT @return_Hex_value = '04'
+								END
+							ELSE
+								BEGIN
+									SET @responseMessage='User status undefined'
+									SELECT @return_Hex_value = 'FE'
+								END
+					END
 					END
 				ELSE
 				-- Didn't find the user using email or PAN or National ID
 				-- Wrong Email
 					BEGIN
-						SET @responseMessage='Invalid Email / PAN / National ID'
+						SET @responseMessage='No user found with given Email or PAN or National ID'
 						SELECT @return_Hex_value = '01'
 						return 1
 					END
 			END TRY
 			BEGIN CATCH
-					SELECT @return_Hex_value='FF', @responseMessage=ERROR_MESSAGE()
+					SELECT @return_Hex_value='FF', @responseMessage='CATCH BLOCK: ' + ERROR_MESSAGE()
 					return -1;
 			END CATCH
 		END
 	ELSE 
 		return -1
 END
+GO
+EXEC usp_Employee_Login  @EmailOrPAN='mxitpewz.fasigoi@jfukjzhbr.jhvwti.net',@HashPassword = '123456789'
