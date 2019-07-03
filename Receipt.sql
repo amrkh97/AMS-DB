ea0c8acef0691dee9bc0bb120cb4283e89b0d19a
amrwy98 <ahmed.m.amrawy@gmail.com>
2019-07-03 12:07:52 +0200

    Receipt SPs modifications

--- Receipt.sql
+++ Receipt.sql
@@ -65,74 +65,177 @@ BEGIN TRY
 		return -1	
 ---------------------------------------------------------------------------
 GO
-create proc usp_Receipt_SelectByRespSQN  @RespSQN NVARCHAR(64)
+create proc usp_Receipt_SelectByRespSQN  @RespSQN NVARCHAR(64),
+    @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
+
 as
+BEGIN TRY
 	IF (@RespSQN IS NOT NULL)
 	BEGIN
 		select * from Receipt
 		where RespSQN = @RespSQN AND ReceiptStatus=0
+ SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
 	END
 	ELSE
-		RETURN -1
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+			SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 
-------------------------------------------------------------------------------
+		
 GO
-create proc usp_Receipt_SelectByCasheirSSN  @CasheirSSN INT
+create proc usp_Receipt_SelectByCasheirSSN  @CasheirSSN INT,
+    @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
+
 as
+BEGIN TRY
 	IF (@CasheirSSN IS NOT NULL)
 	BEGIN
 		select * from Receipt
 		where CasheirSSN = @CasheirSSN AND ReceiptStatus=0
+ SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
 	END
 	ELSE
-		RETURN -1
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+			SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 
-------------------------------------------------------------------------------
+		
 GO
-create proc usp_Receipt_SelectByFTPFileLocation  @FTPFileLocation NVARCHAR(128)
+create proc usp_Receipt_SelectByFTPFileLocation  @FTPFileLocation NVARCHAR(128),
+ @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
+
 as
+BEGIN TRY
 	IF (@FTPFileLocation IS NOT NULL)
 	BEGIN
 		select * from Receipt
 		where FTPFileLocation = @FTPFileLocation AND ReceiptStatus=0
+ SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
 	END
 	ELSE
-		RETURN -1
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+			SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 
-------------------------------------------------------------------------------
+		
 GO
-create proc usp_Receipt_SelectByReceiptStatus  @ReceiptStatus NVARCHAR(64)
+create proc usp_Receipt_SelectByReceiptStatus  @ReceiptStatus NVARCHAR(64),
+ @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
+
 as
+BEGIN TRY
 	IF (@ReceiptStatus IS NOT NULL)
 	BEGIN
 		select * from Receipt
 		where ReceiptStatus = @ReceiptStatus AND ReceiptStatus=0
+	 SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
 	END
 	ELSE
-		RETURN -1
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+			SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 
-------------------------------------------------------------------------------
+		
 GO
-create proc usp_Receipt_SelectByCost  @Cost NVARCHAR(64)
+create proc usp_Receipt_SelectByCost  @Cost NVARCHAR(64),
+ @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
+
 as
+BEGIN TRY
 	IF (@Cost IS NOT NULL)
 	BEGIN
 		select * from Receipt
 		where Cost = @Cost AND ReceiptStatus=0
+	 SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
 	END
 	ELSE
-		RETURN -1
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+			SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 
-------------------------------------------------------------------------------
+		
 GO
-create proc usp_Receipt_SelectByPaymentMethod  @PaymentMethod NVARCHAR(64)
+create proc usp_Receipt_SelectByPaymentMethod  @PaymentMethod NVARCHAR(64),
+ @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
+
 as
+BEGIN TRY
 	IF (@PaymentMethod IS NOT NULL)
 	BEGIN
 		select * from Receipt
 		where PaymentMethod = @PaymentMethod AND ReceiptStatus=0
+	 SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
 	END
 	ELSE
-		RETURN -1
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+			SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 
-------------------------------------------------------------------------------
+		
+GO
