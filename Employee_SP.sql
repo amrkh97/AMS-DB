8921b186471a8a16f6e1c912c74ed7f4d6d12ed2
amrkh97 <amrkh97@gmail.com>
2019-07-04 15:09:59 +0200

    Added Getting Employees by various status

--- Employee_SP.sql
+++ Employee_SP.sql
@@ -1,6 +1,15 @@
 USE KAN_AMO
 GO
 
+CREATE OR ALTER PROC get_Employee_AllParamedics
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 2
+
+END
+GO
 
 CREATE OR ALTER PROC get_Employee_Paramedics
 AS
@@ -8,6 +17,26 @@ BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
 WHERE JobID = 2 AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
+
+END
+GO
+
+CREATE OR ALTER PROC get_Employee_InActiveParamedics
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 2 AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' ) 
+
+END
+
+GO
+CREATE OR ALTER PROC get_Employee_AllDrivers
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 3
 
 END
 
@@ -18,5 +47,15 @@ BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
 WHERE JobID = 3 AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
+
+END
+
+GO
+CREATE OR ALTER PROC get_Employee_InActiveDrivers
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 3 AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' )
 
 END
--- createStoredProcedures.sql
+++ createStoredProcedures.sql
@@ -2197,6 +2197,15 @@ as
 
 GO
 
+CREATE OR ALTER PROC get_Employee_AllParamedics
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 2
+
+END
+GO
 
 CREATE OR ALTER PROC get_Employee_Paramedics
 AS
@@ -2204,6 +2213,26 @@ BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
 WHERE JobID = 2 AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
+
+END
+GO
+
+CREATE OR ALTER PROC get_Employee_InActiveParamedics
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 2 AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' ) 
+
+END
+
+GO
+CREATE OR ALTER PROC get_Employee_AllDrivers
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 3
 
 END
 
@@ -2214,5 +2243,15 @@ BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
 WHERE JobID = 3 AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
+
+END
+
+GO
+CREATE OR ALTER PROC get_Employee_InActiveDrivers
+AS
+BEGIN
+
+SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
+WHERE JobID = 3 AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' )
 
 END
