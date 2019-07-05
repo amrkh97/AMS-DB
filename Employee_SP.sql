7ae1ab29f1643c8cd04d70bcd9834783c475bfb6
amrkh97 <amrkh97@gmail.com>
2019-07-05 10:03:25 +0200

    Added Missing Employee functionalities

--- Employee_SP.sql
+++ Employee_SP.sql
@@ -2,60 +2,66 @@ USE KAN_AMO
 GO
 
 CREATE OR ALTER PROC get_Employee_AllParamedics
+@SuperSSN INT
 AS
 BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
-WHERE JobID = 2
+WHERE JobID = 2 AND SuperSSN = @SuperSSN
 
 END
 GO
 
 CREATE OR ALTER PROC get_Employee_Paramedics
+@SuperSSN INT
 AS
 BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
-WHERE JobID = 2 AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
+WHERE JobID = 2 AND SuperSSN = @SuperSSN AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
 
 END
 GO
 
 CREATE OR ALTER PROC get_Employee_InActiveParamedics
+@SuperSSN INT
 AS
 BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
-WHERE JobID = 2 AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' ) 
+WHERE JobID = 2 AND SuperSSN = @SuperSSN AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' ) 
 
 END
 
 GO
 CREATE OR ALTER PROC get_Employee_AllDrivers
+@SuperSSN INT
 AS
 BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
-WHERE JobID = 3
+WHERE JobID = 3 AND SuperSSN = @SuperSSN
 
 END
 
 GO
 CREATE OR ALTER PROC get_Employee_Drivers
+@SuperSSN INT
 AS
 BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
-WHERE JobID = 3 AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
+WHERE JobID = 3 AND SuperSSN = @SuperSSN AND (EmployeeStatus = '00' OR EmployeeStatus = '01') 
 
 END
 
 GO
 CREATE OR ALTER PROC get_Employee_InActiveDrivers
+@SuperSSN INT
 AS
 BEGIN
 
 SELECT EID,Fname,Lname,Email,ContactNumber,PAN,NationalID,EmployeeStatus,Photo,Age FROM dbo.Employee
-WHERE JobID = 3 AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' )
+WHERE JobID = 3 AND SuperSSN = @SuperSSN AND (EmployeeStatus = '02' OR EmployeeStatus = '03'  OR EmployeeStatus = '04' )
 
 END
