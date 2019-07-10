dda23f37b2f97fa25ff252618f5a8f11aaa7dd46
KamelMohsen <kamelmohsenkamel@gmail.com>
2019-07-10 16:18:18 +0200

    added MR SP

--- Combined Stored Procedures File.sql
+++ Combined Stored Procedures File.sql
@@ -1001,7 +1001,9 @@ BEGIN CATCH
 
 -- (2) Search Unique ID --
 
-----------------------------------------NEW SET OF STORED PROCEDURES--------------------------------------------------------------
+----------------------------------------MEDICAL RECORD STORED PROCEDURES --------------------------------------------------------------
+
+USE KAN_AMO
 
 GO
 CREATE OR ALTER proc usp_MedicalRecord_SelectAll 
@@ -1031,7 +1033,7 @@ as
 		RETURN -1
 
 GO
-CREATE OR ALTER proc usp_MedicalRecord_SelectByStatus  @MRStatus INT
+CREATE OR ALTER proc usp_MedicalRecord_SelectByStatus  @MRStatus NVARCHAR(32)
 as
 	IF (@MRStatus IS NOT NULL)
 	BEGIN
@@ -1042,7 +1044,7 @@ as
 		RETURN -1
 		GO
 CREATE OR ALTER PROC usp_MedicalRecord_Insert 
-	@RespSQN NVARCHAR(64),
+	@RespSQN INT,
 	@PatientID INT,
 	@BloodType NVARCHAR(12),
 	@BloodPressure NVARCHAR(2),
@@ -1070,17 +1072,26 @@ CREATE OR ALTER PROC usp_MedicalRecord_Insert
 	@RecommendedProcedure NVARCHAR(MAX),
 	@MRStatus NVARCHAR(32),
 	@responseCode NVARCHAR(2)='FF' OUTPUT,
-	@responseMessage NVARCHAR(128)='' OUTPUT
-
+	@responseMessage NVARCHAR(128)='' OUTPUT,
+	@mrID INT = 0  OUTPUT
 	
 	as 
 	BEGIN TRY
+	IF(@RespSQN IS NOT NULL AND @PatientID IS NOT NULL )
 		BEGIN
 			INSERT INTO MedicalRecord (RespSQN,PatientID,BloodType,BloodPressure,Diabetes,RespiratoryDiseases,Cancer,CardiovascularDiseases,COPD,Pregnancy,Other,Dead,Consciousness,Breathing,Capillaries,Pulse,BloodPressureLevel,DiabetesLevel,BodyTemp,BreathingRate,CapillariesLevel,Injury,PhysicalExaminationImage,MedicineApplied,ProcedureDoneInCar,RecommendedProcedure,MRStatus)
 			values(@RespSQN,@PatientID,@BloodType,@BloodPressure,@Diabetes,@RespiratoryDiseases,@Cancer,@CardiovascularDiseases,@COPD,@Pregnancy,@Other,@Dead,@Consciousness,@Breathing,@Capillaries,@Pulse,@BloodPressureLevel,@DiabetesLevel,@BodyTemp,@BreathingRate,@CapillariesLevel,@Injury,@PhysicalExaminationImage,@MedicineApplied,@ProcedureDoneInCar,@RecommendedProcedure,@MRStatus)
 		    SELECT @responseCode = '00'
-		    SELECT @responseMessage = 'Success'
-		END
+		    SELECT @responseMessage = 'Successfully Added Medical Record'
+	        SELECT @mrID = (SELECT MedicalRecordID FROM MedicalRecord WHERE RespSQN = @RespSQN AND PatientID=@PatientID )
+			END
+			
+	ELSE
+	BEGIN
+				return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'Unknown Error'
+			END
 	END TRY
 	BEGIN CATCH
 			SELECT @responseCode = 'FF',
@@ -1092,11 +1103,16 @@ CREATE OR ALTER PROC usp_MedicalRecord_Insert
 
 		-- (4) Update MedicalRecord --
 
+
+
+
+		-- (4) Update MedicalRecord --
+
 GO
 CREATE OR ALTER PROC usp_MedicalRecord_Update
 
     @MedicalRecordID INT,
-	@RespSQN NVARCHAR(64),
+	@RespSQN INT,
 	@PatientID INT,
 	@BloodType NVARCHAR(12),
 	@BloodPressure NVARCHAR(2),
@@ -1134,37 +1150,42 @@ Begin TRY
 			UPDATE MedicalRecord
 			SET   
 			
-			RespSQN=ISNULL(RespSQN,@RespSQN),
-			PatientID=ISNULL(PatientID,@PatientID),
-			BloodType=ISNULL(BloodType,@BloodType),
-			BloodPressure=ISNULL(BloodPressure,@BloodPressure),
-			Diabetes=ISNULL(Diabetes,@Diabetes),
-			RespiratoryDiseases=ISNULL(RespiratoryDiseases,@RespiratoryDiseases),
-			Cancer=ISNULL(Cancer,@Cancer),
-			CardiovascularDiseases=ISNULL(CardiovascularDiseases,@CardiovascularDiseases),
-			COPD=ISNULL(COPD,@COPD),
-			Pregnancy=ISNULL(Pregnancy,@Pregnancy),
-			Other=ISNULL(Other,@Other),
-			Dead=ISNULL(Dead,@Dead),
-			Consciousness=ISNULL(Consciousness,@Consciousness),
-			Breathing=ISNULL(Breathing,@Breathing),
-			Capillaries=ISNULL(@Capillaries ,@Capillaries),
-			Pulse=ISNULL(Pulse,@Pulse),
-			BloodPressureLevel=ISNULL(BloodPressureLevel,@BloodPressureLevel),
-			DiabetesLevel=ISNULL(DiabetesLevel,@DiabetesLevel),
-			BodyTemp=ISNULL(BodyTemp,@BodyTemp),
-			BreathingRate=ISNULL(BreathingRate,@BreathingRate),
-			CapillariesLevel=ISNULL(CapillariesLevel,@CapillariesLevel),
-			Injury=ISNULL(Injury,@Injury),
-			PhysicalExaminationImage=ISNULL(PhysicalExaminationImage,@PhysicalExaminationImage),
-			MedicineApplied=ISNULL(MedicineApplied,@MedicineApplied),
-			ProcedureDoneInCar=ISNULL(ProcedureDoneInCar,@ProcedureDoneInCar),
-			RecommendedProcedure=ISNULL(RecommendedProcedure,@RecommendedProcedure),
-			MRStatus=ISNULL(MRStatus,@MRStatus)
+			RespSQN=ISNULL(@RespSQN,RespSQN),
+			PatientID=ISNULL(@PatientID,PatientID),
+			BloodType=ISNULL(@BloodType,BloodType),
+			BloodPressure=ISNULL(@BloodPressure,BloodPressure),
+			Diabetes=ISNULL(@Diabetes,Diabetes),
+			RespiratoryDiseases=ISNULL(@RespiratoryDiseases,RespiratoryDiseases),
+			Cancer=ISNULL(@Cancer,Cancer),
+			CardiovascularDiseases=ISNULL(@CardiovascularDiseases,CardiovascularDiseases),
+			COPD=ISNULL(@COPD,COPD),
+			Pregnancy=ISNULL(@Pregnancy,Pregnancy),
+			Other=ISNULL(@Other,Other),
+			Dead=ISNULL(@Dead,Dead),
+			Consciousness=ISNULL(@Consciousness,Consciousness),
+			Breathing=ISNULL(@Breathing,Breathing),
+			Capillaries=ISNULL(@Capillaries ,Capillaries),
+			Pulse=ISNULL(@Pulse,Pulse),
+			BloodPressureLevel=ISNULL(@BloodPressureLevel,BloodPressureLevel),
+			DiabetesLevel=ISNULL(@DiabetesLevel,DiabetesLevel),
+			BodyTemp=ISNULL(@BodyTemp,BodyTemp),
+			BreathingRate=ISNULL(@BreathingRate,BreathingRate),
+			CapillariesLevel=ISNULL(@CapillariesLevel,CapillariesLevel),
+			Injury=ISNULL(@Injury,Injury),
+			PhysicalExaminationImage=ISNULL(@PhysicalExaminationImage,PhysicalExaminationImage),
+			MedicineApplied=ISNULL(@MedicineApplied,MedicineApplied),
+			ProcedureDoneInCar=ISNULL(@ProcedureDoneInCar,ProcedureDoneInCar),
+			RecommendedProcedure=ISNULL(@RecommendedProcedure,RecommendedProcedure),
+			MRStatus=ISNULL(@MRStatus,MRStatus)
 			
 			WHERE MedicalRecordID = @MedicalRecordID
 		    SELECT @responseCode = '00'
-		    SELECT @responseMessage = 'Success'
+		    SELECT @responseMessage = 'Successfuly Updated'
+			if NOT EXISTS(SELECT * from MedicalRecord WHERE MedicalRecordID = @MedicalRecordID )
+			Begin
+		    SELECT @responseCode = '05'
+		    SELECT @responseMessage = 'Not Found'
+			END
 		
 		END
 		ELSE
@@ -1183,17 +1204,33 @@ Begin TRY
 
 		-- (5) Delete MedicalRecord By ID --
 GO
-CREATE OR ALTER proc usp_MedicalRecord_Delete  @MedicalRecordID  INT
+CREATE OR ALTER proc usp_MedicalRecord_Delete  @MedicalRecordID  INT,@responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
 as
+Begin TRY
 	IF (@MedicalRecordID IS NOT NULL)
 	BEGIN
-		UPDATE  MedicalRecord 
-		SET MRStatus = '99'
-		where MedicalRecordID = @MedicalRecordID
-	END
-	ELSE 
-		RETURN -1
 
+		UPDATE  MedicalRecord 
+		SET MRStatus = 'FF' 
+		where MedicalRecordID = @MedicalRecordID AND NOT MRStatus = 'FF'  
+		 SELECT @responseCode = '00'
+		 SELECT @responseMessage = 'Successfuly Deleted'
+    END
+ 
+		ELSE
+	BEGIN
+				return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'Unknown Error'
+			END
+	END TRY
+	BEGIN CATCH
+			SELECT @responseCode = 'FF',
+			@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
 ----------------------------------------NEW SET OF STORED PROCEDURES--------------------------------------------------------------
 ------------------------- Stored Procedures ----------------------------
 ------------------------------------------------------------------------
