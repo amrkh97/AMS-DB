27c8e75726e7f829fb88513b07068e16142370bb
Ahmed.Gohary <ahmed.gohary>
2019-07-12 20:26:13 +0400

    Changed the separate file

--- Android_IncidentResponse.sql
+++ Android_IncidentResponse.sql
@@ -1,4 +1,5 @@
-USE KAN_AMO;
+USE KAN_AMO
+
 GO
 CREATE OR ALTER PROC usp_getAndroidIncident
 @VIN INT,
@@ -79,20 +80,6 @@ SELECT @alarmLevelName = AlarmLevelName,
        @alarmLevelNote = AlarmLevelNote 
        FROM  dbo.AlarmLevels WHERE AlarmLevelID = @alarmLevelID
 
-
-SELECT @batchID = BatchID from Batch where BatchID = @batchID
+SELECT @BatchID = BatchID FROM dbo.AmbulanceMap WHERE dbo.AmbulanceMap.VIN = @VIN AND StatusMap = '00'
 
 END
-
-GO
-
-CREATE OR ALTER PROC usp_IncidentResponse_GetYelloPad
-@VIN INTEGER,
-@UniqueID NVARCHAR(64) OUTPUT
-AS
-BEGIN
-SELECT @UniqueID = YelloPadUniqueID FROM dbo.Yellopad
-INNER JOIN dbo.AmbulanceMap ON AmbulanceMap.YelloPadID = Yellopad.YelloPadID
-WHERE dbo.AmbulanceMap.VIN = @VIN AND dbo.AmbulanceMap.StatusMap = '00'
-END
-GO
