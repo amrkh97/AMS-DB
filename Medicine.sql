5368f07257c92212fa7ebd9dd377b0fca9d805df
Unknown <unknown@example.com>
2019-07-12 18:39:26 +0300

    medicine SP

--- Medicine.sql
+++ Medicine.sql
@@ -113,20 +113,166 @@ BEGIN CATCH
 	END CATCH
 		
 	return -1
-		
 
 
 
+		GO
+Create  OR ALTER PROC usp_Medicines_SelectAll 
+as
+	select * from Medicine
+	WHERE  MedicineStatus <>'FF'
+	OR CountInStock <> 0
+-- (2.1) Get Medicine By Name --
+GO
+create  OR ALTER PROC usp_Medicine_SelectByName  @MedName NVARCHAR(64)
+as
+	IF (@MedName IS NOT NULL)
+	BEGIN
+		select * from Medicine
+		where MedicineName = @MedName
+	END
+	ELSE
+		RETURN -1
+-- (2.2) Get Medicine By Bar Code --
+GO
+create  OR ALTER PROC usp_Medicine_SelectByBCode  @bCode NVARCHAR(64)
+as
+	IF (@bCode IS NOT NULL)
+		BEGIN
+			select * from Medicine
+			where BarCode = @bCode
+		END
+	ELSE
+		RETURN -1
+-- (2.3) Get Medicine By Status --
+GO
+create  OR ALTER PROC usp_Medicine_SelectBySts @MStatus NVARCHAR(32)
+as
+	select * from Medicine
+	where MedicineStatus = @MStatus
+-- (3) Insert Medicine --
+GO
+CREATE  OR ALTER PROC usp_Medicine_Insert 
+	@BarCode NVARCHAR(64),
+	@Name NVARCHAR(64),
+    @CountInStock NVARCHAR(64) = NULL,
+    @Price NVARCHAR(32) = NULL,
+    @Implications NVARCHAR(MAX) = NULL,
+    @MedicineUsage NVARCHAR(MAX) = NULL,
+    @SideEffects NVARCHAR(MAX) = NULL,
+    @ActiveComponent NVARCHAR(MAX) = NULL,
+	 @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
 
+as
+BEGIN TRY
 
+	IF (@BarCode IS NOT NULL AND @Name IS NOT NULL)
+ BEGIN
+	 IF Exists (Select * from Medicine where BarCode=@BarCode ) 
+	       BEGIN      
+				SELECT @responseCode = '01';
+				SELECT @responseMessage = 'Allready exists';
+				RETURN -1;
+           END
+     ELSE
+          BEGIN
+		        INSERT INTO Medicine (BarCode,CountInStock,MedicineName,Price,Implications,MedicineUsage,SideEffects,ActiveComponent)
+			    VALUES (@BarCode,@CountInStock,@Name,@Price,@Implications,@MedicineUsage,@SideEffects,@ActiveComponent)
+		        SELECT @responseCode = '00'
+		        SELECT @responseMessage = 'Success'
+	      END
+	
+	END
+ELSE
+       BEGIN      
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'BarCode IS  NULL or Name IS NULL'
+				RETURN -1
+           END
 
+	END TRY
+BEGIN CATCH
+		 	SELECT @responseCode = 'FF',
+		    @responseMessage=ERROR_MESSAGE()
+			 RETURN -1;
+	END CATCH
+			 return -1;
+-- (4) Update Medicine --
+GO
+CREATE  OR ALTER PROC usp_Medicine_Update
+	@BarCode NVARCHAR(64),
+	@Name NVARCHAR(64) = NULL,
+    @CountInStock NVARCHAR(64) = NULL,
+    @Price NVARCHAR(32)  = NULL,
+    @Implications NVARCHAR(MAX) = NULL,
+    @MedicineUsage NVARCHAR(MAX) = NULL,
+    @SideEffects NVARCHAR(MAX) = NULL,
+    @ActiveComponent NVARCHAR(MAX) = NULL,
+     @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
 
+as
+BEGIN TRY	 
+IF (@BarCode IS NOT NULL)
+		BEGIN
+			UPDATE Medicine
+			SET CountInStock = ISNULL(@CountInStock,CountInStock),
+			MedicineName = ISNULL(@Name,MedicineName),
+			Price = ISNULL(@Price,Price),
+			Implications = ISNULL(@Implications,Implications),
+			MedicineUsage = ISNULL(@MedicineUsage,MedicineUsage),
+			SideEffects = ISNULL(@SideEffects,SideEffects),
+			ActiveComponent = ISNULL(@ActiveComponent,ActiveComponent),
+			MedicineStatus = 2
+			WHERE BarCode = @BarCode
+		SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
+	END
+	ELSE
+	BEGIN 
+	return -1
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+	END
+	END TRY
+BEGIN CATCH
+		 	SELECT @responseCode = 'FF',
+		@responseMessage=ERROR_MESSAGE()
+			return -1;
+	END CATCH
+		return -1
+-- (5) Delete Medicine By Barcode --
+GO
+create  OR ALTER PROC usp_Medicine_Delete  
+	@BarCode NVARCHAR(64),
+    @responseCode NVARCHAR(2)='FF' OUTPUT,
+	@responseMessage NVARCHAR(128)='' OUTPUT
 
+AS
+BEGIN TRY	 
+IF (@BarCode IS NOT NULL)
+		BEGIN
+			UPDATE Medicine
+			SET  MedicineStatus = 'FF'
+			WHERE BarCode = @BarCode
+		SELECT @responseCode = '00'
+		SELECT @responseMessage = 'Success'
+	END
+	ELSE
+	BEGIN 
+				SELECT @responseCode = 'FF'
+				SELECT @responseMessage = 'nO PARAMETER'
+     	        RETURN -1
 
-
-
-
-
+	END
+	END TRY
+BEGIN CATCH
+		 	SELECT @responseCode = 'FF',
+		     @responseMessage=ERROR_MESSAGE()
+			 RETURN -1;
+	END CATCH
+		 RETURN -1
 
 
 
